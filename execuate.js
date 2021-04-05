a1a24a9a6b9ae222d8b070c4d609c7fa9044a91e
jiwenyoung <youngjiwen@gmail.com>
2021-04-06 03:01:11 +0800

    change field of type

--- execuate.js
+++ execuate.js
@@ -3,6 +3,7 @@ module.exports = (self) => {
         return async () => {
             try {
                 const result = (await self.http.get(self.url)).data
+                result['request_type'] = self.type
                 return Object.create(Data).init(result, self.type)
             } catch (error) {
                 throw error;
